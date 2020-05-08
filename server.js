c81eea46bbbbe0ee4749dcbc9f6212252d8ec734
iamkhattar <shivamkhattar1@gmail.com>
2020-05-08 18:00:07 +0100

    Socket IO initialised

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+
 const port = 3000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () =>
+server.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
