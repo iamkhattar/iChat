1761bd20e8b412a8b6bcb2d67747f0bd0e0ffb1c
iamkhattar <shivamkhattar1@gmail.com>
2020-05-09 01:55:47 +0100

    Chat Theme Created

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Chat from "./components/Chat/Chat";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -11,6 +12,7 @@ const App = () => (
     <Route path="/" exact component={Login} />
     <Switch>
       <Route path="/register" component={Register} />
+      <Route path="/chat" component={Chat} />
     </Switch>
   </Router>
 );
