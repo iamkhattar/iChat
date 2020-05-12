e2f3dd00e04b75a93e8f615ebb81569968074506
kalqallaf <kalqallaf1@outlook.com>
2020-05-12 03:10:34 +0300

    reg

--- controllers/regiser.controller.js
+++
@@ -1,9 +0,0 @@
-// const User = require("../models/User");
-// const bcrypt = require("bcryptjs");
-
-// exports.create = async (req, res, next) => {
-//     await User.findOne({ email: req.body.email })
-//         .then(email_exists => {
-//             return res.status(409)
-//         });
-// }
